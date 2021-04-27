6721b5c94510ae1fe5d5fa0b7743929b57c22d2f
simonsymhoven <post@simon-symhoven.de>
2021-04-27 17:47:55 +0200

    Implement demo monitors views for ux-sig meeting

---
+++ src/main/java/io/jenkins/plugins/monitoring/examples/First.java
@@ -0,0 +1,77 @@
+package io.jenkins.plugins.monitoring.examples;
+
+import edu.hm.hafner.echarts.JacksonFacade;
+import edu.hm.hafner.echarts.Palette;
+import edu.hm.hafner.echarts.PieChartModel;
+import edu.hm.hafner.echarts.PieData;
+import hudson.Extension;
+import hudson.model.Run;
+import io.jenkins.plugins.monitoring.MonitorFactory;
+import io.jenkins.plugins.monitoring.MonitorView;
+
+import java.util.*;
+
+/**
+ * An example Monitor View.
+ */
+public class First implements MonitorView {
+    private final Run<?, ?> build;
+    private final String id;
+
+    /**
+     * Create a new {@link First}.
+     *
+     * @param run
+     *          the {@link Run}
+     * @param viewId
+     *          the id.
+     */
+    public First(Run<?, ?> run, String viewId) {
+        this.build = run;
+        this.id = viewId;
+    }
+
+    @Override
+    public String getTitle() {
+        return "First Monitor " + this.build.getDisplayName();
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    public String getChartId() {
+        return UUID.randomUUID().toString();
+    }
+
+    /**
+     * Generates a new {@link PieChartModel}.
+     *
+     * @return
+     *          the model for pie chart.
+     */
+    public String getModel() {
+        PieChartModel model = new PieChartModel("Title");
+        Random r = new Random();
+        model.add(new PieData("Segment 1", r.nextInt(100)), Palette.RED);
+        model.add(new PieData("Segment 2",  r.nextInt(100)), Palette.GREEN);
+        model.add(new PieData("Segment 3",  r.nextInt(100)), Palette.YELLOW);
+
+        return new JacksonFacade().toJson(model);
+    }
+
+    /**
+     * Creates a new {@link FirstFactory}.
+     */
+    @Extension
+    public static class FirstFactory implements MonitorFactory {
+        @Override
+        public Collection<MonitorView> getMonitorViews(Run<?, ?> build) {
+            List<MonitorView> monitors = new ArrayList<>();
+            monitors.add(new First(build, "io.jenkins.plugins.monitoring.examples.First.View1"));
+            monitors.add(new First(build, "io.jenkins.plugins.monitoring.examples.First.View2"));
+            return monitors;
+        }
+    }
+}
---
+++ src/main/java/io/jenkins/plugins/monitoring/examples/Second.java
@@ -0,0 +1,62 @@
+package io.jenkins.plugins.monitoring.examples;
+
+import edu.hm.hafner.echarts.JacksonFacade;
+import edu.hm.hafner.echarts.PercentagePieChart;
+import hudson.Extension;
+import hudson.model.Run;
+import io.jenkins.plugins.monitoring.MonitorFactory;
+import io.jenkins.plugins.monitoring.MonitorView;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * Another example Monitor View.
+ */
+public class Second implements MonitorView {
+    private final Run<?, ?> build;
+
+    /**
+     * Create a new {@link Second}.
+     *
+     * @param run
+     *          the {@link Run}
+     */
+    public Second(Run<?, ?> run) {
+        this.build = run;
+    }
+
+    @Override
+    public String getTitle() {
+        return "Second Monitor " + build.getDisplayName();
+    }
+
+    @Override
+    public String getId() {
+        return getClass().getName();
+    }
+
+    /**
+     * Generates a new {@link PercentagePieChart}.
+     *
+     * @param percentage
+     *          the percentage for pie chart.
+     *
+     * @return
+     *          the model for pie chart.
+     */
+    public String getProgressModel(final int percentage) {
+        return new JacksonFacade().toJson(new PercentagePieChart().create(percentage));
+    }
+
+    /**
+     * Creates a new {@link SecondFactory}.
+     */
+    @Extension
+    public static class SecondFactory implements MonitorFactory {
+        @Override
+        public Collection<MonitorView> getMonitorViews(Run<?, ?> build) {
+            return Collections.singleton(new Second(build));
+        }
+    }
+}
