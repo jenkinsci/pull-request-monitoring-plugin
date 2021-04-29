d9005be7bb7f252a1086ae2526bfcde950fe05d6
simonsymhoven <post@simon-symhoven.de>
2021-04-29 09:41:20 +0200

    add a user property to store configuration per user

---
+++ src/main/java/io/jenkins/plugins/monitoring/MonitorUserProperty.java
@@ -0,0 +1,93 @@
+package io.jenkins.plugins.monitoring;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.Extension;
+import hudson.model.User;
+import hudson.model.UserProperty;
+import hudson.model.UserPropertyDescriptor;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * A {@link hudson.model.UserProperty} to store the json configuration per user as property.
+ */
+public class MonitorUserProperty extends hudson.model.UserProperty {
+
+    private Collection<MonitorProperty> properties;
+
+    public MonitorUserProperty() {
+
+    }
+
+    public MonitorUserProperty(MonitorProperty defaultProp) {
+        this.properties = new ArrayList<>();
+        this.properties.add(defaultProp);
+    }
+
+    public Collection<MonitorProperty> getProperties() {
+        return properties;
+    }
+
+    public MonitorProperty getView(String id) {
+        return this.getProperties().stream()
+                .filter(monitorProperty -> monitorProperty.getId().equals(id))
+                .findFirst().orElse(null);
+    }
+
+    public void setProperties(Collection<MonitorProperty> properties) {
+        this.properties = properties;
+    }
+
+    public void createOrUpdate(String id, String config) {
+        MonitorProperty property = this.getProperties().stream()
+                .filter(monitorProperty -> monitorProperty.getId().equals(id))
+                .findFirst().orElse(null);
+
+        if (property == null) {
+            this.getProperties().add(new MonitorProperty(id, config));
+        } else {
+            property.setConfig(config);
+        }
+    }
+
+    public static class MonitorProperty {
+
+        private final String id;
+        private String config;
+
+        public MonitorProperty(String id, String config) {
+            this.id = id;
+            this.config = config;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getConfig() {
+            return config;
+        }
+
+        public void setConfig(String config) {
+            this.config = config;
+        }
+
+    }
+
+    @Extension
+    public static class MonitorPropertyDescriptor extends UserPropertyDescriptor {
+
+        @Override
+        public UserProperty newInstance(User user) {
+            return new MonitorUserProperty();
+        }
+
+        @NonNull
+        @Override
+        public String getDisplayName() {
+            return "Monitoring Configuration";
+        }
+    }
+
+}
--- src/main/java/io/jenkins/plugins/monitoring/MonitoringBuildAction.java
+++ src/main/java/io/jenkins/plugins/monitoring/MonitoringBuildAction.java
@@ -1,8 +1,13 @@
 package io.jenkins.plugins.monitoring;
 
 import hudson.model.Run;
+import hudson.model.User;
 import jenkins.model.RunAction2;
 import org.kohsuke.stapler.bind.JavaScriptMethod;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This action displays a link on the side panel of a {@link Run}. The action is only displayed if the parent job
@@ -26,14 +31,59 @@ public class MonitoringBuildAction implements RunAction2 {
      * @param monitor
      *          the {@link Monitor} to be add.
      */
-    public MonitoringBuildAction(Run<?, ?> run, Monitor monitor) {
+    public MonitoringBuildAction(Run<?, ?> run, Monitor monitor) throws IOException {
         this.owner = run;
         this.monitor = monitor;
     }
 
     @JavaScriptMethod
-    public String getConfiguration() {
-        return monitor.getConfiguration();
+    public void updateUserConfiguration(String id, String config) throws IOException {
+        User user = User.current();
+
+        if (user == null) {
+           return;
+        }
+
+        MonitorUserProperty property = user.getProperty(MonitorUserProperty.class);
+        MonitorUserProperty.MonitorProperty defaultProp = new MonitorUserProperty.MonitorProperty("default",
+                this.monitor.getConfiguration());
+
+        if (property == null) {
+            property = new MonitorUserProperty(defaultProp);
+            user.addProperty(property);
+        }
+
+        if (property.getProperties() == null) {
+            List<MonitorUserProperty.MonitorProperty> views = new ArrayList<>();
+            views.add(defaultProp);
+            property.setProperties(views);
+        }
+
+        property.createOrUpdate(id, config);
+        user.save();
+    }
+
+    @JavaScriptMethod
+    public String getConfiguration(String id) throws IOException {
+        User user = User.current();
+
+        if (user == null) {
+            return this.monitor.getConfiguration();
+        }
+
+        MonitorUserProperty property = user.getProperty(MonitorUserProperty.class);
+
+        if (property.getProperties() == null) {
+            updateUserConfiguration("default", this.monitor.getConfiguration());
+        }
+
+        MonitorUserProperty.MonitorProperty monitorProperty = property.getView(id);
+
+        if (monitorProperty == null) {
+            monitorProperty = property.getView("default");
+        }
+
+        return monitorProperty.getConfig();
     }
 
     @Override
@@ -51,14 +101,6 @@ public class MonitoringBuildAction implements RunAction2 {
         return MonitoringMultibranchProjectAction.getURI();
     }
 
-    public Run<?, ?> getRun() {
-        return owner;
-    }
-
-    public Monitor getMonitor() {
-        return monitor;
-    }
-
     @Override
     public void onAttached(Run<?, ?> run) {
         this.owner = run;
@@ -67,6 +109,14 @@ public class MonitoringBuildAction implements RunAction2 {
     @Override
     public void onLoad(Run<?, ?> run) {
         this.owner = run;
+    }
+
+    public Run<?, ?> getRun() {
+        return owner;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
     }
 
 }
