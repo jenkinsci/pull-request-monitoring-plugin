0a35267d80c67715f6b490d9d4f573c5bcf8cbd3
simonsymhoven <post@simon-symhoven.de>
2021-05-04 20:36:38 +0200

    add missing tests for Monitor.java

--- src/test/java/io/jenkins/plugins/monitoring/MonitorTest.java
+++ src/test/java/io/jenkins/plugins/monitoring/MonitorTest.java
@@ -1,12 +1,20 @@
 package io.jenkins.plugins.monitoring;
 
-import hudson.triggers.SCMTrigger;
-import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import hudson.model.Result;
+import jenkins.branch.BranchSource;
+import jenkins.scm.impl.mock.MockSCMController;
+import jenkins.scm.impl.mock.MockSCMSource;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 
 /**
  * Unit tests for the {@link Monitor} step.
@@ -19,16 +27,85 @@ public class MonitorTest {
     public JenkinsRule jenkinsRule = new JenkinsRule();
 
     @Test
-    public void should() throws Exception {
-        WorkflowJob job = jenkinsRule.createProject(WorkflowJob.class, "Pull Request");
-        job.addTrigger(new SCMTrigger(""));
-        job.setDefinition(new CpsFlowDefinition("" +
-                "node {" +
-                "   stage ('Pull Request Monitoring - Empty Dashboard Configuration') {" +
-                "       monitoring ( )" +
-                "   }" +
-                "}", true));
-        WorkflowRun run = jenkinsRule.assertBuildStatusSuccess(job.scheduleBuild2(0));
-        jenkinsRule.assertLogContains("Configuration: {\"plugins\": {}}" , run);
+    public void should_AddDefaultMonitor_When_BuildIsPr() throws Exception {
+        WorkflowMultiBranchProject project = createRepositoryWithPr("Jenkinsfile.default");
+
+        project.scheduleBuild2(0);
+        jenkinsRule.waitUntilNoActivity();
+
+        final WorkflowJob job = project.getItems().iterator().next();
+        final WorkflowRun build = job.getLastBuild();
+        MonitoringBuildAction action = build.getAction(MonitoringBuildAction.class);
+
+        jenkinsRule.assertBuildStatusSuccess(build);
+        jenkinsRule.assertLogContains("Build is part of a pull request. Add monitor now." , build);
+        Assert.assertNotNull(action);
+        Assert.assertEquals(action.getMonitor().getConfiguration(), "{\"plugins\": {}}");
+    }
+
+    @Test
+    public void should_SkipAddDefaultMonitor_When_BuildIsNotPr() throws Exception {
+        WorkflowMultiBranchProject project = createRepositoryWithoutPr("Jenkinsfile.default");
+
+        project.scheduleBuild2(0);
+        jenkinsRule.waitUntilNoActivity();
+
+        final WorkflowJob job = project.getItems().iterator().next();
+        final WorkflowRun build = job.getLastBuild();
+        MonitoringBuildAction action = build.getAction(MonitoringBuildAction.class);
+
+        jenkinsRule.assertBuildStatusSuccess(build);
+        jenkinsRule.assertLogContains("Build is not part of a pull request. Skip adding monitor." , build);
+        Assert.assertNull(action);
+    }
+
+    @Test
+    public void should_ThrowException_When_AddingNotExistingView() throws Exception {
+        WorkflowMultiBranchProject project = createRepositoryWithPr("Jenkinsfile.custom2");
+
+        project.scheduleBuild2(0);
+        jenkinsRule.waitUntilNoActivity();
+
+        final WorkflowJob job = project.getItems().iterator().next();
+        final WorkflowRun build = job.getLastBuild();
+        MonitoringBuildAction action = build.getAction(MonitoringBuildAction.class);
+
+        jenkinsRule.assertBuildStatus(Result.FAILURE, build);
+        jenkinsRule.assertLogContains(
+                "Can't find class 'io.jenkins.plugins.view' in list of available plugins!" , build);
+        Assert.assertNull(action);
+    }
+
+    private WorkflowMultiBranchProject createRepositoryWithPr(String jenkinsfile) throws IOException {
+        MockSCMController controller = MockSCMController.create();
+        controller.createRepository("scm-repo");
+        controller.createBranch("scm-repo", "master");
+        final int num = controller.openChangeRequest("scm-repo", "master");
+        final String crNum = "change-request/" + num;
+        InputStream st = getClass().getResourceAsStream(String.format("/io/jenkins/plugins/monitoring/%s", jenkinsfile));
+        controller.addFile("scm-repo", crNum, "Jenkinsfile", "Jenkinsfile",
+                st.readAllBytes());
+
+        WorkflowMultiBranchProject project = jenkinsRule.createProject(WorkflowMultiBranchProject.class);
+        project.getSourcesList().add(new BranchSource(new MockSCMSource("1", controller, "scm-repo",
+                false, false, true)));
+
+
+        return project;
+    }
+
+    private WorkflowMultiBranchProject createRepositoryWithoutPr(String jenkinsfile) throws IOException {
+        MockSCMController controller = MockSCMController.create();
+        controller.createRepository("scm-repo");
+        controller.createBranch("scm-repo", "master");
+        InputStream st = getClass().getResourceAsStream(String.format("/io/jenkins/plugins/monitoring/%s", jenkinsfile));
+        controller.addFile("scm-repo", "master", "Jenkinsfile", "Jenkinsfile",
+                st.readAllBytes());
+
+        WorkflowMultiBranchProject project = jenkinsRule.createProject(WorkflowMultiBranchProject.class);
+        project.getSourcesList().add(new BranchSource(new MockSCMSource("1", controller, "scm-repo",
+                true, false, false)));
+
+        return project;
     }
 }
