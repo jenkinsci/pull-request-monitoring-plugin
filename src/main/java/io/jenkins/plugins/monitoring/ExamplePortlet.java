22503a44873fe9d4c771c7c2113ae45a8b35762b
simonsymhoven <post@simon-symhoven.de>
2021-05-17 16:54:57 +0200

    implement demo portlet for release

--- src/main/java/io/jenkins/plugins/monitoring/ExamplePortlet.java
+++ src/main/java/io/jenkins/plugins/monitoring/ExamplePortlet.java
@@ -1,11 +1,33 @@
 package io.jenkins.plugins.monitoring;
 
+import hudson.Extension;
+import hudson.model.Run;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
+/**
+ * An example of {@link MonitorPortletFactory}.
+ */
 public class ExamplePortlet implements MonitorPortlet {
+    private final String id;
+    private final String title;
+
+    public ExamplePortlet(String title, String id) {
+        this.id = id;
+        this.title = title;
+    }
+
     @Override
     public String getTitle() {
-        return "Example Portlet";
+        return title;
+    }
+
+    @Override
+    public String getId() {
+        return id;
     }
 
     @Override
@@ -20,11 +42,31 @@ public class ExamplePortlet implements MonitorPortlet {
 
     @Override
     public String getIconUrl() {
-        return "</path-to-icon/icon.png>";
+        return MonitoringMultibranchProjectAction.getIconSmall();
     }
 
     @Override
     public Optional<String> getDetailViewUrl() {
-        return Optional.of("<link-to-detail-view>");
+        return Optional.empty();
+    }
+
+    /**
+     * An example of {@link MonitorPortletFactory}.
+     */
+    @Extension
+    public static class ExampleMonitorFactory implements MonitorPortletFactory {
+
+        @Override
+        public Collection<MonitorPortlet> getPortlets(Run<?, ?> build) {
+            List<MonitorPortlet> portlets = new ArrayList<>();
+            portlets.add(new ExamplePortlet("Good First Portlet", "first-demo-portlet"));
+            portlets.add(new ExamplePortlet("Another Portlet", "second-demo-portlet"));
+            return portlets;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Pull Request Monitoring (Demo)";
+        }
     }
 }
